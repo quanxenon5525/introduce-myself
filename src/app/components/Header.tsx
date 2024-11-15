853ac2637a50c729a23f39ebcd3ec289d3dfecc6
quan.nguyen <quan.nguyen@nexpando.com>
2024-11-15 14:35:00 +0700

    add mobile view for drawer

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import { useMobile } from "../context/MobileViewContext";
-import { DarkThemeToggle } from "flowbite-react";
+import { Button, DarkThemeToggle, Drawer, Sidebar } from "flowbite-react";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { isMobile } = useMobile();
   const [showMobileView, setShowMobileView] = useState(false);
 
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
+  const handleClose = () => {
+    setIsOpen(false);
   };
 
   useEffect(() => {
@@ -19,23 +19,11 @@ const Header = () => {
     <>
       {showMobileView ? (
         <>
-          <div className="flex justify-between items-center mb-5 px-3">
+          <div className="flex justify-between items-center">
             <div>
-              <img src="/logo/menu.svg" alt="menu" width={24} height={24} />
-            </div>
-            <a href="#">
-              <img
-                src="/logo.png"
-                className="object-cover mt-0"
-                alt="logo"
-                width={80}
-                height={80}
-              />
-            </a>
-            <div>
-              <button className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
-                <DarkThemeToggle />
-              </button>
+              <Button onClick={() => setIsOpen(true)} className="bg-white">
+                <img src="/logo/menu.svg" alt="menu" width={24} height={24} />
+              </Button>
             </div>
           </div>
         </>
@@ -87,6 +75,61 @@ const Header = () => {
           </div>
         </div>
       )}
+      <Drawer open={isOpen} onClose={handleClose}>
+        <Drawer.Header
+          className="flex justify-center"
+          title=""
+          titleIcon={() => (
+            <a href="#">
+              <img
+                src="/logo.png"
+                className="object-cover mt-0"
+                alt="logo"
+                width={80}
+                height={80}
+              />
+            </a>
+          )}
+        />
+        <Drawer.Items>
+          <Sidebar
+            aria-label="Sidebar with multi-level dropdown example"
+            className="[&>div]:bg-transparent [&>div]:p-0"
+          >
+            <div className="flex flex-col justify-center space-x-4 w-full items-center">
+              <a
+                href="#"
+                className="text-base header-text transition duration-0 hover:duration-150 ease-in-out"
+              >
+                Home
+              </a>
+              <a
+                href="#"
+                className="text-base header-text duration-0 hover:duration-150 ease-in-out"
+              >
+                About me
+              </a>
+              <a
+                href="#"
+                className="text-basel header-text duration-0 hover:duration-150 ease-in-out"
+              >
+                Project
+              </a>
+              <a
+                href="#"
+                className="text-base header-text duration-0 hover:duration-150 ease-in-out"
+              >
+                Contact
+              </a>
+              <div className="flex items-center">
+                <button className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
+                  <DarkThemeToggle />
+                </button>
+              </div>
+            </div>
+          </Sidebar>
+        </Drawer.Items>
+      </Drawer>
     </>
   );
 };
